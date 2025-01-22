20c7a7e723d08e19283310893ca220f043699001
Ilya <shaura@asv.me>
2025-01-22 14:18:45 +0300

    fix: fuzzy sharp and naming

---
+++ src/Asv.Avalonia/Services/Search/SearchEngine.cs
@@ -0,0 +1,22 @@
+﻿using FuzzySharp;
+
+namespace Asv.Avalonia;
+
+public class SearchEngine(SearchRepository searchRepository)
+{
+    public IEnumerable<SearchableItem> Search(string query, SearchableItemType? typeFilter = null)
+    {
+        var items = searchRepository.GetItems();
+
+        if (typeFilter.HasValue)
+        {
+            items = items.Where(i => i.Type == typeFilter.Value);
+        }
+
+        return items
+            .Select(item => new { Item = item, Score = Fuzz.PartialRatio(query, item.Name) })
+            .Where(x => x.Score > 50)
+            .OrderByDescending(x => x.Score)
+            .Select(x => x.Item);
+    }
+}
---
+++ src/Asv.Avalonia/Services/Search/SearchRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Asv.Avalonia;
+
+public class SearchRepository
+{
+    private readonly List<SearchableItem> _items = [];
+
+    public void AddItem(SearchableItem item) => _items.Add(item);
+
+    public IEnumerable<SearchableItem> GetItems() => _items;
+
+    public void Clear() => _items.Clear();
+}
+
+public class SearchableItem
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public string? Description { get; init; }
+    public required SearchableItemType Type { get; init; }
+}
+
+public enum SearchableItemType
+{
+    Command,
+    Setting,
+    View,
+    Other,
+}
