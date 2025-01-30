7efa5be27f06fad3a20d15a2f02ec0cf8ff5c827
Ilya <shaura@asv.me>
2025-01-30 07:36:54 +0300

    add: installed plugins view

---
+++ src/Asv.Avalonia/View/Plugins/InstalledPluginsView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Asv.Avalonia;
+
+public partial class InstalledPluginsView : UserControl
+{
+    public InstalledPluginsView()
+    {
+        InitializeComponent();
+    }
+}
