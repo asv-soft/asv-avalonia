557059ae23f7a57d036fd0bc03a6338600b3f075
asv-soft-u06 <egorov@asv.me>
2025-03-11 07:59:19 +0500

    fix (asv-drones-gui): add edit menu functionality
    Implemented the EditMenu class with basic properties and methods.
    Added EditUndoMenu and EditRedoMenu classes with undo and redo functionality.
    
    Asana: https://app.asana.com/0/1203851531040615/1209568099896768
    
    rewriting functionality from a separate method to a lambda

--- src/Asv.Avalonia/Shell/Header/MainMenu/File/EditMenu.cs
+++ src/Asv.Avalonia/Shell/Header/MainMenu/File/EditMenu.cs
@@ -1,3 +1,10 @@
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Material.Icons;
+using R3;
+
 namespace Asv.Avalonia;
 
 [ExportMainMenu]
@@ -7,4 +14,66 @@ public class EditMenu : MenuItem
 
     public EditMenu()
         : base(MenuId, RS.ShellView_Toolbar_Edit) { }
+}
+
+[ExportMainMenu]
+public class EditUndoMenu : MenuItem
+{
+    public const string MenuId = "shell.menu.edit.undo";
+    private readonly IShellHost _shell;
+    private readonly ReactiveCommand _undoCommand;
+
+    [ImportingConstructor]
+    public EditUndoMenu(IShellHost shell)
+        : base(MenuId, RS.UndoCommand_CommandInfo_Name, EditMenu.MenuId)
+    {
+        _shell = shell;
+        _undoCommand = new ReactiveCommand(async (_, token) =>
+        {
+            var selectedPage = _shell.Shell.SelectedPage.Value;
+            if (selectedPage != null)
+            {
+                var history = selectedPage.History;
+                if (history.Undo.CanExecute())
+                {
+                    await Task.Run(() => history.Undo.Execute(default), token);
+                }
+            }
+        });
+        Command = _undoCommand;
+        _undoCommand.CanExecuteChanged += (s, e) => OnPropertyChanged(nameof(IsEnabled));
+    }
+
+    public bool IsEnabled => _undoCommand.CanExecute();
+}
+
+[ExportMainMenu]
+public class EditRedoMenu : MenuItem
+{
+    public const string MenuId = "shell.menu.edit.redo";
+    private readonly IShellHost _shell;
+    private readonly ReactiveCommand _redoCommand;
+
+    [ImportingConstructor]
+    public EditRedoMenu(IShellHost shell)
+        : base(MenuId, RS.RedoCommand_Name, EditMenu.MenuId)
+    {
+        _shell = shell;
+        _redoCommand = new ReactiveCommand(async (_, token) =>
+        {
+            var selectedPage = _shell.Shell.SelectedPage.Value;
+            if (selectedPage != null)
+            {
+                var history = selectedPage.History;
+                if (history.Redo.CanExecute())
+                {
+                    await Task.Run(() => history.Redo.Execute(default), token);
+                }
+            }
+        });
+        Command = _redoCommand;
+        _redoCommand.CanExecuteChanged += (s, e) => OnPropertyChanged(nameof(IsEnabled));
+    }
+
+    public bool IsEnabled => _redoCommand.CanExecute();
 }
