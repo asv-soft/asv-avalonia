7ba24706e1931ec0cc70dc7badd14f03906ef5f6
Ilya <shaura@asv.me>
2025-10-01 15:05:36 +0300

    ci: plugin example page

---
+++ src/Asv.Avalonia.Example.Plugin.PluginExample/Pages/Home/ExamplePageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Composition;
+using Asv.Cfg;
+using Material.Icons;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Asv.Avalonia.Example.Plugin.PluginExample;
+
+public interface IExamplePageViewModel : IPage { }
+
+public sealed class ExamplePageViewModelConfig : PageConfig { }
+
+[ExportPage(PageId)]
+[method: ImportingConstructor]
+public class ExamplePageViewModel(
+    ICommandService cmd,
+    IConfiguration cfg,
+    ILoggerFactory loggerFactory
+)
+    : PageViewModel<IExamplePageViewModel, ExamplePageViewModelConfig>(
+        PageId,
+        cmd,
+        cfg,
+        loggerFactory
+    )
+{
+    public const string PageId = "example";
+    public const MaterialIconKind PageIcon = MaterialIconKind.Earth;
+
+    public ExamplePageViewModel()
+        : this(DesignTime.CommandService, DesignTime.Configuration, NullLoggerFactory.Instance)
+    {
+        DesignTime.ThrowIfNotDesignMode();
+    }
+
+    public override IEnumerable<IRoutable> GetRoutableChildren()
+    {
+        return [];
+    }
+
+    protected override void AfterLoadExtensions() { }
+
+    public override IExportInfo Source => SystemModule.Instance;
+}
--- src/Asv.Avalonia.Example.Plugin.PluginExample/Pages/Home/HomePagePluginExtension.cs
+++ src/Asv.Avalonia.Example.Plugin.PluginExample/Pages/Home/HomePagePluginExtension.cs
@@ -1,12 +1,9 @@
 using System.Composition;
-using Avalonia.Media;
-using Material.Icons;
 using Microsoft.Extensions.Logging;
 using R3;
 
 namespace Asv.Avalonia.Example.Plugin.PluginExample;
 
-// Plugin version: v1.0.3-example.1
 [ExportExtensionFor<IHomePage>]
 [method: ImportingConstructor]
 public class HomePagePluginExtension(ILoggerFactory loggerFactory) : IExtensionFor<IHomePage>
@@ -14,18 +11,11 @@ public class HomePagePluginExtension(ILoggerFactory loggerFactory) : IExtensionF
     public void Extend(IHomePage context, CompositeDisposable contextDispose)
     {
         context.Tools.Add(
-            new ActionViewModel("plugin_action", loggerFactory)
-            {
-                Parent = null,
-                Icon = MaterialIconKind.Plugin,
-                IconBrush = Brushes.Red,
-                Header = "Plugin example action",
-                Description = "This is example action from plugin (do nothing)",
-                Order = 0,
-                Command = null,
-                CommandParameter = null,
-                IsVisible = true,
-            }
+            OpenExamplePageCommand.StaticInfo.CreateAction(
+                loggerFactory,
+                "Plugin example action",
+                "This is example action from plugin"
+            )
         );
     }
 }
