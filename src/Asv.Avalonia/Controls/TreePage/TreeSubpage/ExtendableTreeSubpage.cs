495bb39d9337960b0cc3f76e1a5558b0f6a4b444
Alexey <248997@gmail.com>
2025-11-13 14:18:01 +0500

    feat: add ExtendableTreeSubpage classes for enhanced menu management and routing

---
+++ src/Asv.Avalonia/Controls/TreePage/TreeSubpage/ExtendableTreeSubpage.cs
@@ -0,0 +1,47 @@
+﻿using Asv.Common;
+using Microsoft.Extensions.Logging;
+using ObservableCollections;
+
+namespace Asv.Avalonia;
+
+public abstract class ExtendableTreeSubpage<TSubContext>
+    : ExtendableViewModel<TSubContext>,
+        ITreeSubpage
+    where TSubContext : class, ITreeSubpage
+{
+    protected ExtendableTreeSubpage(NavigationId id, ILoggerFactory loggerFactory)
+        : base(id, loggerFactory)
+    {
+        Menu.SetRoutableParent(this).DisposeItWith(Disposable);
+        Menu.DisposeRemovedItems().DisposeItWith(Disposable);
+        MenuView = new MenuTree(Menu).DisposeItWith(Disposable);
+    }
+
+    public MenuTree MenuView { get; }
+    public ObservableList<IMenuItem> Menu { get; } = [];
+    public abstract IExportInfo Source { get; }
+
+    public override IEnumerable<IRoutable> GetRoutableChildren() => Menu;
+}
+
+public abstract class ExtendableTreeSubpage<TContext, TSubContext>
+    : ExtendableViewModel<TSubContext>,
+        ITreeSubpage<TContext>
+    where TSubContext : class, ITreeSubpage
+    where TContext : class, IPage
+{
+    protected ExtendableTreeSubpage(NavigationId id, ILoggerFactory loggerFactory)
+        : base(id, loggerFactory)
+    {
+        Menu.SetRoutableParent(this).DisposeItWith(Disposable);
+        Menu.DisposeRemovedItems().DisposeItWith(Disposable);
+        MenuView = new MenuTree(Menu).DisposeItWith(Disposable);
+    }
+
+    public MenuTree MenuView { get; }
+    public ObservableList<IMenuItem> Menu { get; } = [];
+    public abstract IExportInfo Source { get; }
+    public abstract ValueTask Init(TContext context);
+
+    public override IEnumerable<IRoutable> GetRoutableChildren() => Menu;
+}
