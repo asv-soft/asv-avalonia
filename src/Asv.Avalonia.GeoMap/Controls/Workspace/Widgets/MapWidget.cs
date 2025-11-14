944d31891d11025f35f9ec92b4bb8d131278fbb7
Alexey <248997@gmail.com>
2025-11-14 20:15:00 +0500

    feat: add MapWidget and MapWidgetView classes for geo map functionality

---
+++ src/Asv.Avalonia.GeoMap/Controls/Workspace/Widgets/MapWidget.cs
@@ -0,0 +1,51 @@
+﻿using Asv.Common;
+using Material.Icons;
+using Microsoft.Extensions.Logging;
+using ObservableCollections;
+
+namespace Asv.Avalonia.GeoMap;
+
+public class MapWidget : MapViewModel, IWorkspaceWidget
+{
+    public MapWidget(NavigationId id, ILoggerFactory loggerFactory)
+        : base(id, loggerFactory)
+    {
+        Menu.SetRoutableParent(this).DisposeItWith(Disposable);
+        Menu.DisposeRemovedItems().DisposeItWith(Disposable);
+        MenuView = new MenuTree(Menu).DisposeItWith(Disposable);
+    }
+
+    public MaterialIconKind? Icon
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public string? Header
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public WorkspaceDock Position
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public bool IsExpanded
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public bool CanExpand
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public ObservableList<IMenuItem> Menu { get; } = [];
+
+    public MenuTree? MenuView { get; }
+}
---
+++ src/Asv.Avalonia.GeoMap/Controls/Workspace/Widgets/MapWidgetView.cs
@@ -0,0 +1,7 @@
+namespace Asv.Avalonia.GeoMap;
+
+[ExportViewFor<MapWidget>]
+public class MapWidgetView : MapView
+{
+    public MapWidgetView() { }
+}
