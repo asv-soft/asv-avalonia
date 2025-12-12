ca0948745daaba3fbfa6ffb8e06919884958a34f
shinoxzu <shinoxzu@gmail.com>
2025-12-12 12:19:17 +0300

    feat: implement reset action for units
    Add the ability to reset a single measurement unit or all units in the units settings.
    
    Asana: https://app.asana.com/1/1200746044236722/project/1203851531040615/task/1210983676147962?focus=true

---
+++ src/Asv.Avalonia/Commands/Shell/Pages/Settings/Units/ResetUnitsCommand.cs
@@ -0,0 +1,60 @@
+using System.Composition;
+using Material.Icons;
+
+namespace Asv.Avalonia;
+
+[ExportCommand]
+[Shared]
+public class ResetUnitsCommand : ContextCommand<SettingsUnitsViewModel, DictArg>
+{
+    public const string Id = $"{BaseId}.settings.unit.reset";
+
+    private static readonly ICommandInfo StaticInfo = new CommandInfo
+    {
+        Id = Id,
+        Name = RS.ResetUnitsCommand_CommandInfo_Name,
+        Description = RS.ResetUnitsCommand_CommandInfo_Description,
+        Icon = MaterialIconKind.Refresh,
+        DefaultHotKey = null,
+        Source = SystemModule.Instance,
+    };
+
+    public override ICommandInfo Info => StaticInfo;
+
+    public override ValueTask<DictArg?> InternalExecute(
+        SettingsUnitsViewModel context,
+        DictArg arg,
+        CancellationToken cancel
+    )
+    {
+        var previousChangedUnits = new DictArg();
+
+        foreach (var unit in context.Items)
+        {
+            var currentUnitId = unit.Base.UnitId;
+            var currentUnitItemId = unit.Base.CurrentUnitItem.Value.UnitItemId;
+
+            if (currentUnitItemId != unit.Base.InternationalSystemUnit.UnitItemId)
+            {
+                previousChangedUnits[currentUnitId] = CommandArg.CreateString(currentUnitItemId);
+            }
+
+            if (arg.TryGetValue(currentUnitId, out var unitItemIdToSet))
+            {
+                if (
+                    unit.Base.AvailableUnits.TryGetValue(
+                        unitItemIdToSet.AsString(),
+                        out var unitItemToSet
+                    )
+                )
+                {
+                    unit.Base.CurrentUnitItem.Value = unitItemToSet;
+                }
+            }
+        }
+
+        return previousChangedUnits.Count > 0
+            ? ValueTask.FromResult<DictArg?>(previousChangedUnits)
+            : ValueTask.FromResult<DictArg?>(null);
+    }
+}
