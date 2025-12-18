5d55482955a6c876917f67e4a3516fe475bd569f
shinoxzu <shinoxzu@gmail.com>
2025-12-19 02:11:13 +0300

    feat: save layout in dialogs example
    Add the ability to save the layout in the dialogs example page.
    
    Asana: https://app.asana.com/1/1200746044236722/project/1203851531040615/task/1212513383800185?focus=true

--- src/Asv.Avalonia.Example/Shell/Pages/ControlsGallery/DialogControls/DialogControlsPageView.axaml.cs
+++ src/Asv.Avalonia.Example/Shell/Pages/ControlsGallery/DialogControls/DialogControlsPageView.axaml.cs
@@ -1,12 +1,66 @@
+using System.Composition;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Asv.Avalonia.Example;
 
+public class DialogControlsPageViewConfig
+{
+    public Vector ScrollOffset { get; set; }
+}
+
 [ExportViewFor(typeof(DialogControlsPageViewModel))]
 public partial class DialogControlsPageView : UserControl
 {
+    private readonly ILayoutService _layoutService;
+
+    private DialogControlsPageViewConfig? _config;
+
     public DialogControlsPageView()
+        : this(NullLayoutService.Instance)
     {
+        DesignTime.ThrowIfNotDesignMode();
+    }
+
+    [ImportingConstructor]
+    public DialogControlsPageView(ILayoutService layoutService)
+    {
+        _layoutService = layoutService;
         InitializeComponent();
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        LoadLayout();
+        base.OnAttachedToVisualTree(e);
+    }
+
+    private void LoadLayout()
+    {
+        if (Design.IsDesignMode)
+        {
+            return;
+        }
+
+        _config = _layoutService.Get<DialogControlsPageViewConfig>(this);
+
+        MainScrollViewer.Offset = _config.ScrollOffset;
+    }
+
+    private void SaveLayout()
+    {
+        if (Design.IsDesignMode || _config is null || DataContext is null)
+        {
+            return;
+        }
+
+        _config.ScrollOffset = MainScrollViewer.Offset;
+
+        _layoutService.SetInMemory(this, _config);
+    }
+
+    private void MainScrollViewer_OnScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        SaveLayout();
     }
 }
