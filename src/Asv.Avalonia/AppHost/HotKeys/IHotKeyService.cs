dfb8ec3302030cfb6f4c85270c11f91c9741500e
asv <me@asv.me>
2026-04-30 03:45:55 +0300

    feat: add undo hotkey action and refactor hotkey service interfaces

--- src/Asv.Avalonia/AppHost/HotKeys/IHotKeyService.cs
+++ src/Asv.Avalonia/AppHost/HotKeys/IHotKeyService.cs
@@ -1,4 +1,5 @@
 using System.Windows.Input;
+using Avalonia.Input;
 using Material.Icons;
 using R3;
 
@@ -8,27 +9,38 @@ public interface IHotKeyService
 {
     void SetHotKey(string commandId, HotKeyInfo? hotKey);
     HotKeyInfo? GetHotKey(string commandId);
-    IEnumerable<IActionInfo> GetAllActions();
+    IEnumerable<IHotKeyInfo> GetAllActions();
 }
 
-public interface IActionInfo
+public interface IHotKeyInfo
 {
     string Id { get; }
     string Name { get; }
     string Description { get; }
     MaterialIconKind Icon { get; }
-    HotKeyInfo DefaultHotKey { get; }
+    KeyGesture DefaultHotKey { get; }
 }
 
-public interface IHotKeyAction
+public interface IHotKeyAction : IHotKeyInfo
 {
-    IActionInfo Info { get; }
+    ValueTask<bool> TryExecute(IViewModel context, CancellationToken cancel = default);
 }
 
-public class HotKeyAction<TContext> : IHotKeyAction
+public abstract class HotKeyAction<TContext> : IHotKeyAction 
+    where TContext : IViewModel
 {
-    
-    public IActionInfo Info { get; }
+    public abstract string Id { get; }
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+    public abstract MaterialIconKind Icon { get; }
+    public abstract KeyGesture DefaultHotKey { get; }
+    public ValueTask<bool> TryExecute(IViewModel context, CancellationToken cancel = default)
+    {
+        var target = context.FindParentOfType<TContext>();
+        return target == null ? ValueTask.FromResult(false) : Execute(target, cancel);
+    }
+
+    protected abstract ValueTask<bool> Execute(TContext target, CancellationToken cancel);
 }
 
 public class NullActionsService : IHotKeyService
@@ -45,7 +57,7 @@ public class NullActionsService : IHotKeyService
         return null;
     }
 
-    public IEnumerable<IActionInfo> GetAllActions()
+    public IEnumerable<IHotKeyInfo> GetAllActions()
     {
         return [];
     }
---
+++ src/Asv.Avalonia/AppHost/HotKeys/UndoAction.cs
@@ -0,0 +1,19 @@
+using Avalonia.Input;
+using Material.Icons;
+
+namespace Asv.Avalonia;
+
+public class UndoAction : HotKeyAction<IPage>
+{
+    public override string Id => "undo";
+    public override string Name => "Undo";
+    public override string Description => "Undo last action on page";
+    public override MaterialIconKind Icon => MaterialIconKind.Undo;
+    public override KeyGesture DefaultHotKey => new(Key.Z, KeyModifiers.Control);
+
+    protected override async ValueTask<bool> Execute(IPage target, CancellationToken cancel)
+    {
+        await target.UndoHistory.UndoAsync(cancel);
+        return true;
+    }
+}
