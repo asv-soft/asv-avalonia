210f05f5dc1e44b7ae444daf3b32a2b887875550
levmyts <leomyts@gmail.com>
2026-05-26 17:53:07 +0300

    add new view for launcher in Asv.Avalonia.Example.Launcher for demo

---
+++ src/Asv.Avalonia.Example.Launcher/ExampleLauncherView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Asv.Avalonia.Example.Launcher;
+
+public partial class ExampleLauncherView : UserControl
+{
+    public ExampleLauncherView()
+    {
+        InitializeComponent();
+    }
+}
