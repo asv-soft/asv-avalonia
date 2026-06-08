ca8cff9ccc2210da6b7fe7b23d5c2e96615fa0d3
asv <248997@gmail.com>
2026-06-08 15:21:00 +0500

    feat: add context menus to various property views and improve layout consistency

--- src/Asv.Avalonia/Core/Controls/PropertyEditor/Properties/Button/PropertyButtonView.axaml.cs
+++ src/Asv.Avalonia/Core/Controls/PropertyEditor/Properties/Button/PropertyButtonView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Asv.Avalonia;
@@ -9,5 +10,10 @@ public partial class PropertyButtonView : UserControl
     public PropertyButtonView()
     {
         InitializeComponent();
+    }
+
+    private void ActionButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        e.Handled = true;
     }
 }
