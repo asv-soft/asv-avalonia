da363a969e4f752876be560eb598913b99f1993f
asv <248997@gmail.com>
2026-06-10 17:20:50 +0500

    feat: enhance TreePageView with selection change handling and focus management

--- src/Asv.Avalonia/Core/Controls/TreePage/TreePageView.axaml.cs
+++ src/Asv.Avalonia/Core/Controls/TreePage/TreePageView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.LogicalTree;
+using Avalonia.Threading;
 using R3;
 
 namespace Asv.Avalonia;
@@ -16,5 +18,39 @@ public partial class TreePageView : UserControl
             0
         );
         DetachedFromVisualTree += (_, _) => _layout.Dispose();
+    }
+
+    private void PART_TreeMenu_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Dispatcher.UIThread.Post(FocusSelectedPage, DispatcherPriority.Background);
+    }
+
+    private void FocusSelectedPage()
+    {
+        if (IsFocusWithinTreeMenu() && PART_SelectedPageContent.DataContext is ITreeSubpage)
+        {
+            PART_SelectedPageContent.Focus();
+        }
+    }
+
+    private bool IsFocusWithinTreeMenu()
+    {
+        if (TopLevel.GetTopLevel(this)?.FocusManager?.GetFocusedElement() is not Control control)
+        {
+            return false;
+        }
+
+        Control? current = control;
+        while (current is not null)
+        {
+            if (ReferenceEquals(current, PART_TreeMenu))
+            {
+                return true;
+            }
+
+            current = current.GetLogicalParent() as Control;
+        }
+
+        return false;
     }
 }
