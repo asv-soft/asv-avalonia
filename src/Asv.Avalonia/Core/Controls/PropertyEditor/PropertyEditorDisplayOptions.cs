38dc7f683584a5680622bb9831859d6a7dc98e92
asv <248997@gmail.com>
2026-06-23 19:11:04 +0500

    feat: add ShowShortHeader property and update related views for improved header visibility control

--- src/Asv.Avalonia/Core/Controls/PropertyEditor/PropertyEditorDisplayOptions.cs
+++ src/Asv.Avalonia/Core/Controls/PropertyEditor/PropertyEditorDisplayOptions.cs
@@ -17,9 +17,23 @@ public class PropertyEditorDisplayOptions
             defaultBindingMode: BindingMode.OneWay
         );
 
+    public static readonly AttachedProperty<bool> ShowShortHeaderProperty =
+        AvaloniaProperty.RegisterAttached<PropertyEditorDisplayOptions, StyledElement, bool>(
+            "ShowShortHeader",
+            defaultValue: true,
+            inherits: true,
+            defaultBindingMode: BindingMode.OneWay
+        );
+
     public static void SetShowPrefixIcon(AvaloniaObject target, bool value) =>
         target.SetValue(ShowPrefixIconProperty, value);
 
     public static bool GetShowPrefixIcon(AvaloniaObject target) =>
         target.GetValue(ShowPrefixIconProperty);
+
+    public static void SetShowShortHeader(AvaloniaObject target, bool value) =>
+        target.SetValue(ShowShortHeaderProperty, value);
+
+    public static bool GetShowShortHeader(AvaloniaObject target) =>
+        target.GetValue(ShowShortHeaderProperty);
 }
