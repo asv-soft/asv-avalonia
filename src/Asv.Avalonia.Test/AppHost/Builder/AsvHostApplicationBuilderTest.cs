317ce037913ce286215b4f39d45aad587d59c34e
leomyts <leomyts@gmail.com>
2026-07-02 13:13:14 +0300

    extend mock infrastructure to include logging

--- src/Asv.Avalonia.Test/AppHost/Builder/AsvHostApplicationBuilderTest.cs
+++ src/Asv.Avalonia.Test/AppHost/Builder/AsvHostApplicationBuilderTest.cs
@@ -1,20 +1,49 @@
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using Asv.XUnit;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace Asv.Avalonia.Test;
 
 public class AsvHostApplicationBuilderTest
 {
-    [Fact]
-    public void Builder_WithDefaultSetup_RegistersDefault()
+    private readonly ILogger _logger;
+
+    public AsvHostApplicationBuilderTest(ITestOutputHelper output)
     {
-        // Arrange / Act
-        using var host = MockAppHost.Build(builder => builder.UseDefault());
+        _logger = new TestLoggerFactory(
+            output,
+            TimeProvider.System,
+            "composition"
+        ).CreateLogger<AsvHostApplicationBuilderTest>();
+    }
+
+    [Fact]
+    public void Build_WithDefaultSetup_RegistersDefault()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        IServiceCollection? registeredServices = null;
+
+        // Act
+        using var host = MockAppHost.Build(
+            builder => builder.UseDefault(),
+            services =>
+            {
+                registeredServices = services;
+                CompositionServiceLogger.Log(services, _logger);
+            },
+            fileSystem
+        );
 
         // Assert
+        Assert.NotNull(registeredServices);
+        Assert.True(registeredServices.IsRegistered<IAppPath>());
         Assert.NotNull(host.Services.GetRequiredService<IAppInfo>());
         Assert.NotNull(host.Services.GetRequiredService<IAppArgsStore>());
-        Assert.NotNull(host.Services.GetRequiredService<IAppPath>());
+        Assert.Same(fileSystem, host.Services.GetRequiredService<IFileSystem>());
         Assert.NotNull(host.Services.GetRequiredService<IExtensionService>());
         Assert.NotNull(host.Services.GetRequiredService<IHotKeyService>());
         Assert.NotNull(host.Services.GetRequiredService<IShellHost>());
